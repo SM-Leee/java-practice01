c2e9d105049d5cf7a7fb2537bad22483d093e789
BIT <BIT@DESKTOP-E23PFRN>
2018-12-27 08:48:53 +0900

    all change

--- src/practice01/Prob4.java
+++ src/practice01/Prob4.java
@@ -1,9 +1,13 @@
 package practice01;
 
+import java.util.Scanner;
 
 public class Prob4 {
 
 	public static void main(String[] args) {
+		System.out.print("문자열을 입력하세요. : ");
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
 	}
 
 }
--- src/practice01/Prob5.java
+++ src/practice01/Prob5.java
@@ -1,7 +1,23 @@
 package practice01;
 
+import java.util.Scanner;
+
 public class Prob5 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = 1;
+
+		for(int i=1;i<100;i++) {
+			if(i%10==3||i%10==6||i%10==9) {
+				System.out.print(i+"\t짝");
+				if(i/10==3||i/10==6||i/10==9) {
+					System.out.print("짝");
+				}
+				System.out.println();
+			}
+		}
+
+
 	}
 }
