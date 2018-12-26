62729fdc15c8288525447f4e24d079e7d2b886e2
user <user@DESKTOP-N1TUS9L>
2018-12-26 23:26:24 +0900

    Pro3 Change

--- src/practice01/Prob3.java
+++ src/practice01/Prob3.java
@@ -1,7 +1,19 @@
 package practice01;
 
+import java.util.Scanner;
+
 public class Prob3 {
-	
+
 	public static void main(String[] args) {
+
+		System.out.print("숫자를 입력하세요 : ");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+
+		for (int i = a-2; i > 0; i-=2) {
+			a += i;               
+		}
+
+		System.out.println("결과값 : "+a);
 	}
 }
