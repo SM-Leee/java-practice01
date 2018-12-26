6befee7ae227029be7892ffe1a13e4c8510820f1
BIT <BIT@DESKTOP-E23PFRN>
2018-12-26 19:10:07 +0900

    Prob2 Change

--- src/practice01/Prob2.java
+++ src/practice01/Prob2.java
@@ -2,5 +2,12 @@ package practice01;
 
 public class Prob2 {
 	public static void main(String[] args) {
+		
+		for(int i=0;i<9;i++) {
+			System.out.println();
+			for(int j=1;j<=10;j++) {
+				System.out.print(i+j+" ");
+			}
+		}
 	}
 }
