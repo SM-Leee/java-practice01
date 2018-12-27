ab13ae4e047083e11ea031c5451a8e9a2892d23a
BIT <BIT@DESKTOP-E23PFRN>
2018-12-27 11:13:50 +0900

    change Comlplete

--- src/practice01/Prob3.java
+++ src/practice01/Prob3.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Prob3 {
 	
 	public static void main(String[] args) {
+		
 		System.out.print("숫자를 입력하세요 : ");
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
